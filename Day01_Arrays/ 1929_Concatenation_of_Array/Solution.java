900aa86241313558d4e116efb65ee324e0b894b4
Joshuaas08 <joshuaasirsolomon@gmail.com>
2026-09-09 23:54:27 +0530

    Update Solution.java

--- Day01_Arrays/ 1929_Concatenation_of_Array/Solution.java
+++ Day01_Arrays/ 1929_Concatenation_of_Array/Solution.java
@@ -1,1 +1,14 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+
+        int n = nums.length;
+
+        int [] ans = new int[2*n];
+
+        for (int num =0;num<n;num++){
+
+            ans[num]= nums[num];
+            
+            ans[num+n]= nums[num];
+
 
