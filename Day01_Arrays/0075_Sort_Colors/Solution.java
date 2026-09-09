3762f41c3f1165c9acf6ce714f461f64ff23e05b
Joshuaas08 <joshuaasirsolomon@gmail.com>
2026-09-09 22:56:09 +0530

    Update Solution.java

--- Day01_Arrays/0075_Sort_Colors/Solution.java
+++ Day01_Arrays/0075_Sort_Colors/Solution.java
@@ -26,6 +26,9 @@ class Solution {
         }
     }
      private void swap(int[] nums, int i, int j) {
+         int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
         
 
    
