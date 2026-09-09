2c8cf763ecac94028f3b4a8912ae870181e79098
Joshuaas08 <joshuaasirsolomon@gmail.com>
2026-09-09 22:58:43 +0530

    Add duplicateZeros method to handle zero duplication
    Implement a method to duplicate zeros in an array.

--- Day01_Arrays/1089_Duplicate_Zeros/Solution.java
+++ Day01_Arrays/1089_Duplicate_Zeros/Solution.java
@@ -1,1 +1,35 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int n = arr.length;
+
+        // Count the number of zeros that will be duplicated
+        int zeros = 0;
+
+        for (int num : arr) {
+            if (num == 0) {
+                zeros++;
+            }
+        }
+
+        // Start from the end
+        int i = n - 1;
+        int j = n + zeros - 1;
+
+        while (i >= 0) {
+
+            // Only write if j is inside the array
+            if (j < n) {
+                arr[j] = arr[i];
+            }
+            j--;
+
+            // If current element is zero, duplicate it
+            if (arr[i] == 0) {
+                if (j < n) {
+                    arr[j] = 0;
+                }
+                j--;
+            }
+
+            i--;
 
