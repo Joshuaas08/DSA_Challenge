71277aac71bdb8b5404f4a7714d4fdc6d8b609e7
Joshuaas08 <joshuaasirsolomon@gmail.com>
2026-09-11 00:18:53 +0530

    Update Solution.java

--- Day2_Searching_Sorting/Leetcode_Problems/06_KthLargest_215/Solution.java
+++ Day2_Searching_Sorting/Leetcode_Problems/06_KthLargest_215/Solution.java
@@ -1,1 +1,8 @@
+### Approach
+Sort the array in ascending order. The kth largest element is at:
 
+`nums.length - k`
+
+QuickSelect can improve the average time, but a poor pivot can cause O(n²).
+
+  Convert the kth-largest position into an index after sorting.
