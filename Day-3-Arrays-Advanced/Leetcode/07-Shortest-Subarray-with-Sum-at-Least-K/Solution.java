94875c555e58ee40da1a35d4312662411779127c
Joshuaas08 <joshuaasirsolomon@gmail.com>
2026-09-11 11:36:50 +0530

    Update Solution.java

--- Day-3-Arrays-Advanced/Leetcode/07-Shortest-Subarray-with-Sum-at-Least-K/Solution.java
+++ Day-3-Arrays-Advanced/Leetcode/07-Shortest-Subarray-with-Sum-at-Least-K/Solution.java
@@ -1,1 +1,41 @@
+class Solution {
+    public int shortestSubarray(int[] nums, int k) {
 
+        int n = nums.length;
+
+        long[] prefix = new long[n + 1];
+
+        // Build prefix sum
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        int answer = n + 1;
+
+        for (int i = 0; i <= n; i++) {
+
+            // Check if we can form a valid subarray
+            while (!deque.isEmpty()
+                    && prefix[i] - prefix[deque.peekFirst()] >= k) {
+
+                answer = Math.min(
+                    answer,
+                    i - deque.pollFirst()
+                );
+            }
+
+            // Maintain increasing prefix sums
+            while (!deque.isEmpty()
+                    && prefix[i] <= prefix[deque.peekLast()]) {
+
+                deque.pollLast();
+            }
+
+            deque.addLast(i);
+        }
+
+        return answer == n + 1 ? -1 : answer;
+    }
+}
