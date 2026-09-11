53aa0a59bb13c70d7c7ac84508fa722743920922
Joshuaas08 <joshuaasirsolomon@gmail.com>
2026-09-11 11:39:26 +0530

    Update Solution.java

--- Day-3-Arrays-Advanced/Leetcode/06-Make-Sum-Divisible-by-P/Solution.java
+++ Day-3-Arrays-Advanced/Leetcode/06-Make-Sum-Divisible-by-P/Solution.java
@@ -1,1 +1,51 @@
+class Solution {
+    public int minSubarray(int[] nums, int p) {
 
+        // Find total remainder
+        long totalSum = 0;
+
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int target = (int)(totalSum % p);
+
+        // Already divisible
+        if (target == 0) {
+            return 0;
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Remainder 0 before the array starts
+        map.put(0, -1);
+
+        long prefixSum = 0;
+        int minLength = nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            prefixSum += nums[i];
+
+            int remainder = (int)(prefixSum % p);
+
+            // Remainder we need to find
+            int required = (remainder - target + p) % p;
+
+            if (map.containsKey(required)) {
+                int previousIndex = map.get(required);
+
+                minLength = Math.min(
+                    minLength,
+                    i - previousIndex
+                );
+            }
+
+            // Store latest index
+            map.put(remainder, i);
+        }
+
+        // Cannot remove the entire array
+        return minLength == nums.length ? -1 : minLength;
+    }
+}
