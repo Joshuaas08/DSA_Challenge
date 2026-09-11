1326bb331a1f17d8572b94c35eacc7b382ef993a
Joshuaas08 <joshuaasirsolomon@gmail.com>
2026-09-11 11:41:23 +0530

    Update Solution.java

--- Day-3-Arrays-Advanced/Leetcode/03-Contiguous-Array/Solution.java
+++ Day-3-Arrays-Advanced/Leetcode/03-Contiguous-Array/Solution.java
@@ -1,1 +1,39 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
 
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Prefix sum 0 exists before we start
+        map.put(0, -1);
+
+        int prefixSum = 0;
+        int maxLength = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            // Convert 0 to -1
+            if (nums[i] == 0) {
+                prefixSum--;
+            } else {
+                prefixSum++;
+            }
+
+            // If we've seen this prefix sum before
+            if (map.containsKey(prefixSum)) {
+
+                int previousIndex = map.get(prefixSum);
+
+                maxLength = Math.max(
+                    maxLength,
+                    i - previousIndex
+                );
+
+            } else {
+                // Store only the FIRST occurrence
+                map.put(prefixSum, i);
+            }
+        }
+
+        return maxLength;
+    }
+}
